aaa79796fd24e1a3907a676f441bcb4d41a9011a
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-11-19 14:52:04 +0100

    "links item selected and eventlistener added"

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
- import { renderApp} from './functions.js';
-document.addEventListener("DOMContentLoaded",renderApp)
+import { renderApp } from "./functions.js";
+document.addEventListener("DOMContentLoaded", renderApp);
+
+const homeLink = document.getElementById("homeLink");
+const classesLink = document.getElementById("classesLink");
+const teachersLink = document.getElementById("teachersLink");
+const studentsLink = document.getElementById("studentsLink");
+
+homeLink.addEventListener("click", renderContent("home-content"));
+
+classesLink.addEventListener("click", renderContent("classes-content"));
+
+teachersLink.addEventListener("click", renderContent("teachers-content"));
+
+studentsLink.addEventListener("click", renderContent("students-content"));
