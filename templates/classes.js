fcf20583341f5fdff9b29f0949c55b8b4b0175e9
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-11-25 02:02:57 +0100

    "classes content structure created"

--- templates/classes.js
+++ templates/classes.js
@@ -1,3 +1,42 @@
 export const classesContent = `<section id="classes-content">
-  <h4>Classes Content</h4>
+  <div class="container mt-5 mx-5 my-3">
+    <h4>Classes</h4>
+  </div>
+
+  <div class="container d-flex justify-content-center  my-2">
+    <div class="row flex-wrap">
+      <div class="col mb-3">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Fullstack Class</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Hannes Bühler</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Students</a>
+            <a href="#" class="card-link">Teachers</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col ">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Cloud Class</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Ali Sayar</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Students</a>
+            <a href="#" class="card-link">Teachers</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container text-center my-5">
+    <div>
+      <a href="#">
+        <img class="text-white" src="./assets/images/plus-circle-icon.png" alt="Add New Class"></img>
+      </a>
+    </div>
+    <p>Add New Class</p>
+  </div>
 </section>`;
