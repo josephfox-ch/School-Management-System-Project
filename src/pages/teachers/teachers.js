fae502c1c30fa991f5031204c545cbe706ded0d8
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-11-28 18:57:50 +0100

    "fix:  fix teachers title

--- src/pages/teachers/teachers.js
+++ src/pages/teachers/teachers.js
@@ -2,7 +2,7 @@ import { teachersBodyContainer } from "./components/teachers.content.body.js";
 
 export const teachersContent = `<section id="classes-content">
   <div class="container mt-5 mx-5 my-3">
-    <h4>Classes</h4>
+    <h4>Teachers</h4>
   </div>
 
 ${teachersBodyContainer}
