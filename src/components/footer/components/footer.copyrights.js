9b9de507fc657231e2154451c735ce1b90b3f33e
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-11-29 04:18:39 +0100

    "build: add footer link "

--- src/components/footer/components/footer.copyrights.js
+++ src/components/footer/components/footer.copyrights.js
@@ -1,6 +1,6 @@
 
-export const footerCopyRihgts =`<div class="container-fluid  justify-content-end ">
-<p class="mb-0">&copy;Joseph Fox-2023</p>
+export const footerCopyRihgts =`<div class="container-fluid  justify-content-end text-white">
+<p class="mb-0"><a id="j-link" href="https://www.josephfox.ch/" target="blank" style = "link" >&copy;Joseph Fox-2023</a></p>
 </div>`
 
 
--- src/components/footer/components/footer.links.js
+++ src/components/footer/components/footer.links.js
@@ -1,12 +1,12 @@
 export function footerLinksFragment() {
   return `<ul class="navbar-nav">
     <li class="nav-item">
-      <a id="aboutLink"  class="nav-link" data-content-id ="about" href="#">
+      <a id="aboutLink"  class="nav-link text-white" data-content-id ="about" href="#">
         About Us
       </a>
     </li>
     <li class="nav-item">
-      <a id="contactLink"  class="nav-link" data-content-id ="contact" href="#">
+      <a id="contactLink"  class="nav-link text-white" data-content-id ="contact" href="#">
         Contact
       </a>
     </li>
