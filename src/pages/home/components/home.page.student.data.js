e8623d5fb0f6f00e4c7a965aeb2c78b8e542b200
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-11-30 19:49:43 +0100

    "fix: fix home page "

--- src/pages/home/components/home.page.student.data.js
+++ src/pages/home/components/home.page.student.data.js
@@ -1,12 +1,12 @@
-export const homePageStudentsDataFragment = ()=> {
-    return `
+export const homePageStudentsDataFragment = () => {
+  return `
     <div class="col mb-3 mx-auto">
     <div class="card border-info mb-3" style="max-width: 18rem;">
-    <div class="card-header"><h5 class="card-title text-info">Students</h5></div>
+    <div class="card-header"><h5 class="card-title text-info">Number of Students</h5></div>
     <div class="card-body">
-      <h5 class="card-title text-info">Info card title</h5>
+      <h5 class="card-title text-info">Info Students</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  </div>`
-}
+  </div>`;
+};
--- src/pages/home/components/home.page.teacher.data.js
+++ src/pages/home/components/home.page.teacher.data.js
@@ -1,17 +1,12 @@
-export const homePageTeaachersDataFragment = ()=> {
-    return `
+export const homePageTeaachersDataFragment = () => {
+  return `
     <div class="col mb-3 mx-auto">
     <div class="card border-warning mb-3" style="max-width: 18rem;">
-    <div class="card-header"><h5 class="card-title text-warning">Teachers</h5></div>
+    <div class="card-header"><h5 class="card-title text-warning">Number of Teachers</h5></div>
     <div class="card-body">
-      <h5 class="card-title text-warning">Warning card title</h5>
+      <h5 class="card-title text-warning">Info Teachers</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  </div>`
-}
-
-
-
-
-
+  </div>`;
+};
