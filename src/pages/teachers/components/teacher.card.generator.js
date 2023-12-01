190019a6b035b9b3793178df66d064bdbfa04f79
josephfox-ch <142756034+josephfox-ch@users.noreply.github.com>
2023-12-01 16:29:23 +0100

    "style: style teacher card"

--- src/pages/teachers/components/teacher.card.generator.js
+++ src/pages/teachers/components/teacher.card.generator.js
@@ -9,8 +9,8 @@ export function generateTeacherCardsHTML() {
         <div class="col mb-3 mx-auto">
           <div class="card border-danger" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title text-danger">${teacher.teacherName}</h5>
-              <h6 class="card-subtitle mb-2 text-warning">${teacher.expertise}</h6>
+              <h5 class="card-title text-warning">${teacher.teacherName}</h5>
+              <h6 class="card-subtitle mb-2">${teacher.expertise}</h6>
               <p class="card-text">${teacher.data}</p>
               <div class= " d-flex justify-content-around">
                 <a href="#" class="card-link text-info">Students</a>
